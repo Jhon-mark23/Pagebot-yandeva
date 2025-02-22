cc1a2382e5e875e3e0bc8fdeb17686242ee9aa86
Mark <123864853+Jhon-mark23@users.noreply.github.com>
2025-02-22 17:02:09 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const web = require("./website/web.js");
+const webhook = require("./webhook.js");
+const parser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(parser.json());
+app.use(express.static("website"));
+
+app.get("/config.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "config.json"));
+});
+
+app.get("/", (req, res) => {
+  web.html(res);
+});
+
+app.get("/webhook", (req, res) => {
+  web.verify(req, res);
+});
+
+app.post("/webhook", (req, res) => {
+  setTimeout(() => {
+    webhook.listen(req.body);
+  }, 5000);
+  res.sendStatus(200);
+});
+
+module.exports = app;
